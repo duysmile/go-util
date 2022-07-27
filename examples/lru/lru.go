99e805379b1c2d3a3828d08395d9c4fe8b1eb146
Duy Nguyen <duy210697@gmail.com>
2022-07-27 22:32:12 +0700

    add lru example

--- examples/lru/lru.go
+++ examples/lru/lru.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/duysmile/go-util/lru"
+)
 
+func main() {
+	lruCache := lru.NewLRU(2)
+
+	lruCache.Add("1", 1)
+	lruCache.Add("2", 2)
+
+	key := "2"
+	v, _ := lruCache.Get(key)
+	fmt.Printf("Get a cache hit [key]: %s, [value]: %v\n", key, v)
+
+	lruCache.Add("3", 3)
+	key = "1"
+	v, _ = lruCache.Get(key)
+	fmt.Printf("Cannot get cache [key]: %s, [value]: %v because cache reach max entries\n", key, v)
 }
